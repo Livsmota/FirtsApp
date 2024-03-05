9f89ca1b52b67b4e88a0be4c43c2bdc2b8e12aa3
Lívia da Silva Mota Carvalho <livia_mota7@portalsesisp.org.br>
2024-03-05 11:30:53 -0300

    criar um layout para calcular a idade.
     criar um layout para validar um email e telefone.

--- app/src/main/java/com/liviamota/firtsapp/MainActivity.kt
+++ app/src/main/java/com/liviamota/firtsapp/MainActivity.kt
@@ -16,15 +16,15 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        binding.btnEnviar.setOnClickListener {
-            var nome = binding.edtNome.editableText.toString()
-            binding.tvNome.text = "Nome: ${nome}"
-
-            var anoNascimento = binding.edtIdade.editableText.toString()
-            val anoAtual = LocalDateTime.now().year
-            var idade = anoAtual - anoNascimento.toInt()
-
-            binding.tvIdade.text = "Idade: ${idade}"
+//        binding.btnEnviar.setOnClickListener {
+//            var nome = binding.edtNome.editableText.toString()
+//            binding.tvNome.text = "Nome: ${nome}"
+//
+//            var anoNascimento = binding.edtIdade.editableText.toString()
+//            val anoAtual = LocalDateTime.now().year
+//            var idade = anoAtual - anoNascimento.toInt()
+//
+//            binding.tvIdade.text = "Idade: ${idade}"
 
         binding.btnEnviar.setOnClickListener {
             var email = binding.edtEmail.editableText.toString()
