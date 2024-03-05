cc765362694a81757e55fef06d0ef267f8d41d5b
Lívia da Silva Mota Carvalho <livia_mota7@portalsesisp.org.br>
2024-03-05 11:29:26 -0300

    criar um layout para calcular a idade.
     criar um layout para validar um email e telefone.

--- app/src/main/java/com/liviamota/firtsapp/MainActivity.kt
+++ app/src/main/java/com/liviamota/firtsapp/MainActivity.kt
@@ -2,10 +2,46 @@ package com.liviamota.firtsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.liviamota.firtsapp.databinding.ActivityMainBinding
+import com.liviamota.firtsapp.databinding.TelaLinearBinding
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnEnviar.setOnClickListener {
+            var nome = binding.edtNome.editableText.toString()
+            binding.tvNome.text = "Nome: ${nome}"
+
+            var anoNascimento = binding.edtIdade.editableText.toString()
+            val anoAtual = LocalDateTime.now().year
+            var idade = anoAtual - anoNascimento.toInt()
+
+            binding.tvIdade.text = "Idade: ${idade}"
+
+        binding.btnEnviar.setOnClickListener {
+            var email = binding.edtEmail.editableText.toString()
+
+            if (email.contains("@") && email.substringAfter("@").contains(".com")) {
+                binding.tvEmail.text = "Email: ${email}"
+            } else {
+                binding.tvEmail.text = "Email: Error"
+            }
+
+            var telefone = binding.edtTelefone.editableText.toString()
+
+            if (telefone.length == 11) {
+                binding.tvTelefone.text = "Telefone: ${telefone}"
+            } else {
+                binding.tvTelefone.text = "Telefone: Error"
+            }
+        }
     }
 }
